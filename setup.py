a8400e6819b42ebedf490b3ca66b5fb67bcec6fb
Daniel J. Lewis <dlewis33@uwo.ca>
2017-02-06 18:50:51 -0500

    Changes to be committed: 	deleted:    DDApp.py 	deleted:    LICENSE 	deleted:    README.md 	deleted:    _config.yml 	new file:   ddcomp/__init__.py 	new file:   ddcomp/__init__.pyc 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ddcomp',
+    version='0.1',
+    description='Delay discounting calculator',
+    url='https://github.com/nanodan/ddcomp',
+    author='Daniel J. Lewis',
+    license='GNU General Public License v3.0',
+    packages=['ddcomp'],
+    zip_safe=False)
