47948c6eda3cfdac28cb65b706053f85194db5cb
Daniel J. Lewis <dlewis33@uwo.ca>
2017-02-07 10:41:21 -0500

    Changes to be committed: 	copied:     ddcomp/__init__.py -> build/lib/ddcomp/__init__.py 	new file:   ddcomp.egg-info/PKG-INFO 	new file:   ddcomp.egg-info/SOURCES.txt 	new file:   ddcomp.egg-info/dependency_links.txt 	new file:   ddcomp.egg-info/not-zip-safe 	new file:   ddcomp.egg-info/requires.txt 	new file:   ddcomp.egg-info/top_level.txt 	modified:   ddcomp/__init__.py 	new file:   ddcomp/ddcomp.py 	new file:   dist/ddcomp-0.5-py2-none-any.whl 	new file:   dist/ddcomp-0.5.tar.gz 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ddcomp',
-    version='0.1',
+    version='0.5',
     description='Delay discounting calculator',
     url='https://github.com/nanodan/ddcomp',
     author='Daniel J. Lewis',
+    author_email='daniel.jacob.lewis@gmail.com',
     license='GNU General Public License v3.0',
-    packages=['ddcomp'],
+    packages=find_packages(),
+    install_requires=['numpy','scikit-learn','pandas','matplotlib'],
+    package_data={'':['*.txt']},
     zip_safe=False)
